8e88fe5f9b3d4d6e6381f4fc77a80e09a8564927
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-08-08 10:09:08 -0400

    add css and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componetns/home/home.component';
+import { AboutComponent } from './componetns/about/about.component';
+import { ContactComponent } from './componetns/contact/contact.component';
+import { ResumeComponent } from './componetns/resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule
